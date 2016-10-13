afe5f167961ddbc5a6be097c7d433697f42ca0d3
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-10-13 15:55:07 -0400

    fix

--- src/main/java/com/ironyard/Sql/insert.sql
+++ src/main/java/com/ironyard/Sql/insert.sql
@@ -1,5 +1,6 @@
-INSERT INTO public.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (1, 'StarBucks', 'coffee', 5, 8);
-INSERT INTO public.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (2, 'McDonald''s', 'lunch', 4, 6);
-INSERT INTO public.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (3, 'Wendy''s', 'lunch', 6, 4);
-INSERT INTO public.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (4, 'Dunkin Donuts', 'coffee', 3, 5);
-INSERT INTO public.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (5, 'Burger King', 'lunch', 6, 5);
+INSERT INTO finance.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (1, 'Wendy''s', 'Lunch', 5, 6);
+INSERT INTO finance.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (2, 'Goodbye', 'Hello', 456, 123);
+INSERT INTO finance.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (3, 'Goodbye', 'Hello', 456, 123);
+INSERT INTO finance.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (6, 'starbucks', 'coffee', 6, 16);
+INSERT INTO finance.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (7, 'starbucks', 'coffee', 232, 4334);
+INSERT INTO finance.budget (bud_key_id, bud_description, bud_category, bud_budgeted_amount, bud_actual_amount) VALUES (8, 'starbucks', 'coffee', 232, 4334);
